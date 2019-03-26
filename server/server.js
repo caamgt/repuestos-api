3ed1c7245bc7e147446a750a20920f88ba772bd9
nazgulgt <35571206+nazgulgt@users.noreply.github.com>
2019-03-26 09:59:14 -0600

    Test

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors());
 // application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // Configuración global de rutas
 app.use(require('./routes/index'));
 
